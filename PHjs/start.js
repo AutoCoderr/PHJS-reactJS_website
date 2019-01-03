eabf5ecae4df275fc2e4f89f71ffc4bb1698554d
julien <julienbouvet78@hotmail.com>
2019-01-03 12:36:14 +0100

    set launcher

---
+++ PHjs/start.js
@@ -0,0 +1,5 @@
+var PHjs = require("./PHjs");
+
+var libs = {};
+
+PHjs("/root/projects/node+react/backend/","http",8000,{},libs,"/root/projects/node+react/master/PHjs/access.log","/root/projects/node+react/master/PHjs/error.log","/root/projects/node+react/master/PHjs/config.txt");
