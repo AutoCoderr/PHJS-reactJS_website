e46ed5c3e0d62aba9ef2c85b4ef7548ac2d6edd5
Julien <julienbouvet78@hotmail.com>
2019-01-03 08:37:53 +0100

    two step

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { Route, Switch } from 'react-router-dom';
 import { LoginOrLogout } from './components/LoginOrLogout.js';
 import { Signup } from './components/Signup.js';
 import { Fiche } from './components/Fiche.js';
+import { Admin } from './components/Admin.js';
 import { Index } from './components/Index.js';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
+//import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
+import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from "reactstrap";
 import $ from 'jquery';
 import './App.css';
 
@@ -45,6 +47,7 @@ class App extends Component {
                     <Route exact path="/login" component={LoginOrLogout}/>
                     <Route exact path ="/signup" component={Signup} />
                     <Route exact path ="/fiche" component={Fiche} />
+                    <Route exact path ="/admin" component={Admin} />
                 </Switch>
             </div>
         </div>
@@ -57,62 +60,44 @@ function Header() {
 	if (localStorage.getItem("session") != null) {
 		isAuth = 1;
 	}
-	/*return (
-		<div style={{position: 'fixed'},{top: '0px'}} id="header">
-			<center>
-			<a href="/">Racine</a>-<a href="/login">{isAuth == 1 ? "Deconnexion" : "Connexion"}</a>-<a href="/signup">Incription</a>
-			{isAuth == 1 && (<span>-<a href="/fiche" >Fiche</a></span>)}
-			</center>
-		</div>
-	)*/
-
-	/*return (
-		<Navbar>
-  			<Navbar.Header>
-    			<Navbar.Brand>
-      			<a href="/">Site de merde</a>
-    			</Navbar.Brand>
-  			</Navbar.Header>
-  			<Nav pullRight>
-    			<NavItem eventKey={1} href="/login">
-      				{isAuth == 1 ? "Deconnexion" : "Connexion"}
-    			</NavItem>
-    			<NavItem eventKey={2} href="/signup">
-      				Incription
-    			</NavItem>
-    			{isAuth == 1 && (
-    			<NavItem eventKey={2} href="/fiche">
-      				Fiche
-    			</NavItem>
-    			)}
-  			</Nav>
-		</Navbar>
-	)*/
 	return (
 		<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-  			<a class="navbar-brand" title="Sommaire" href="/sommaire.php"><img style={{float: "left"},{marginRight: "10px"},{width: "25px"},{height:"25px"}} src="/img/logo.png" /><span>Castellane-Auto</span></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  			<a class="navbar-brand" title="Sommaire" href="/">
+  				<span>Site de merde</span>
+  			</a>
+  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    		<span class="navbar-toggler-icon"></span>
+  			</button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-          <li class="nav-item" id="presentationli">
-            <a class="nav-link" href="/index.php">Présentation</a>
-          </li>
-          <li class="nav-item" id="tarifli">
-            <a class="nav-link" href="/tarif.php">Tarif</a>
-          </li>
-          <li class="nav-item" id="contactli">
-			<a class="nav-link" href="/contact.php">Contacts</a>
-          </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-        <li class="nav-item" id="registerli"><a class="nav-link" href="/register.php"><span class="glyphicon glyphicon-user"></span>Inscription</a></li>
-    <li class="nav-item" id="loginli"><a class="nav-link" href="/login.php"><span class="glyphicon glyphicon-log-in"></span>connexion</a></li>
-      </ul>
-    </div>
-</nav>
+  			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    			<ul class="navbar-nav mr-auto">
+    				{isAuth == 1 && 
+    					(
+    					<li className={(window.location.href.split("/")[window.location.href.split("/").length-1] === "fiche") ? "active nav-item" : "nav-item"}>
+            				<a class="nav-link" href="/fiche">Fiche</a>
+          				</li>
+    					)
+    				}
+    				{(isAuth == 1 && (JSON.parse(localStorage.getItem("session")).perm == "admin" | JSON.parse(localStorage.getItem("session")).perm == "superadmin")) ? (
+          				<li className={(window.location.href.split("/")[window.location.href.split("/").length-1] === "admin") ? "active nav-item" : "nav-item"}>
+            				<a class="nav-link" href="/admin">Administration</a>
+          				</li>
+          			) : ""}
+            	</ul>
+            	<ul class="nav navbar-nav navbar-right">
+        			<li className={(window.location.href.split("/")[window.location.href.split("/").length-1] === "signup") ? "active nav-item" : "nav-item"}>
+        				<a class="nav-link" href="/signup">
+        					<span class="glyphicon glyphicon-user"></span>
+        						Inscription
+        				</a>
+        			</li>
+
+    				<li className={(window.location.href.split("/")[window.location.href.split("/").length-1] === "login") ? "active nav-item" : "nav-item"}><a class="nav-link" href="/login"><span class="glyphicon glyphicon-log-in">
+    					</span>{isAuth == 1 ? "Deconnexion" : "Connexion"}</a>
+    				</li>
+      			</ul>
+    		</div>
+		</nav>
 	)
 }
 
