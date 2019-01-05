8512dc09d56fbd9d2270ed2d0bb0c15897e2920a
Julien <julienbouvet78@hotmail.com>
2019-01-05 19:14:39 +0100

    add 'isConnect()' in start.js

--- PHjs/start.js
+++ PHjs/start.js
@@ -2,4 +2,13 @@ var PHjs = require("./PHjs");
 
 var libs = {};
 
+libs.isConnect = (sessions,token) => {
+	for (var i=0;i<sessions.length;i++) {
+		if (sessions[i].token == token) {
+			return true;
+		}
+	}
+	return false;
+}
+
 PHjs("/root/projects/node+react/backend/","http",8000,{},libs,"/root/projects/node+react/master/PHjs/access.log","/root/projects/node+react/master/PHjs/error.log","/root/projects/node+react/master/PHjs/config.txt");
